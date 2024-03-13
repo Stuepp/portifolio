4af32c375009605351435c66a74af5d6245ffd26
Stuepp <arthurh.c@hotmail.com>
2024-03-13 13:19:13 -0300

    page projects design changed and Gravadora text updated

--- src/app/components/Card.js
+++ src/app/components/Card.js
@@ -10,23 +10,31 @@ export default function Card(){
   let gainTrackerLink = '/projects/gainTracker'
 
   return (
-    <div>
-      <div className="w-sm border rounded-lg shadow bg-gray-800 border-gray-700">
+    <div className='my-4 flex flex-col lg:flex-row justify-center gap-5 py-4 overflow-hidden sm:gap-8'>
+      <div className="w-sm border rounded-lg shadow bg-gray-800 border-gray-700 m-4">
           <Link href={gravadoraLink}>
             <Image src={gravadora} width={700} height={500} alt='Grabadora diagram' className='rounded-t-lg w-full' />
           </Link>
           <div className="p-5">
             <Link href={gravadoraLink} className="text-sm font-semibold leading-6 text-gray-900">
-              <h5 className="mb-2 text-2xl font-bold tracking-tight text-white">Gravadora</h5>
+              <h5 className="mb-2 text-2xl font-bold tracking-tight text-white">Gravadora - Record Company</h5>
             </Link>
-            <p className="mb-3 font-normal text-gray-400">Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.</p>
+            <p className="mb-3 font-normal text-gray-400">This is a project that used the follow technologies:</p>
+            <ul className="max-w-md space-y-1 text-gray-300 list-disc list-inside text-left m-4">
+              <li>Django</li>
+              <li>HTML</li>
+              <li>CSS</li>
+              <li>Bootstrap</li>
+              <li>Postgres SQL</li>
+              <li>Mongo DB</li>
+            </ul>
             <Link href={gravadoraLink} className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
             Read More
             </Link>
           </div>
       </div>
 
-      <div className="w-sm border rounded-lg shadow bg-gray-800 border-gray-700">
+      <div className="w-sm border rounded-lg shadow bg-gray-800 border-gray-700 m-4">
           <Link href={gainTrackerLink}>
             <Image src={gaintracker} width={700} height={500} alt='Gain Tracker diagram' className='rounded-t-lg w-full' />
           </Link>
--- src/app/projects/page.js
+++ src/app/projects/page.js
@@ -7,9 +7,7 @@ export default function Home(){
             <NavBar />
             <div className="sm:px-8 py-16">
                 <h1 className="text-center text-4xl font-bold tracking-tight text-zinc-100 sm:text-5x">Projects I have  develop for now</h1>
-                <div className="my-4 flex flex-row justify-center gap-5 py-4 overflow-hidden sm:gap-8">
-                    <Card />
-                </div>
+                <Card />
             </div>
         </main>
     )
