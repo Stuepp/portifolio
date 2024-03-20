9c27b9d950634734f2c7670675f159e0b834bb47
Stuepp <arthurh.c@hotmail.com>
2024-03-20 19:28:54 -0300

    fix text color

--- src/app/components/ProojectPoint.js
+++ src/app/components/ProojectPoint.js
@@ -1,8 +1,8 @@
 export default function ProjectPoint({question, answear}){
   return(
     <div>
-      <h1 className="text-2xl">{question}</h1>
-      <p className="mx-auto my-2 max-w-2xl space-y-1 text-gray-300">{answear}</p>
+      <h1 className="text-2xl text-zinc-100">{question}</h1>
+      <p className="mx-auto my-2 max-w-2xl space-y-1 text-zinc-200">{answear}</p>
     </div>
   )
 }
