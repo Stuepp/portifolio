f96531e0aaebe16523dd0aa25507d38edda62f10
Arthur Henrique Cavalcanti <50219237+Stuepp@users.noreply.github.com>
2024-08-24 23:08:29 -0300

    Update page.js

--- src/app/projects/page.js
+++ src/app/projects/page.js
@@ -4,7 +4,7 @@ export default function ProjectsPage(){
     return(
         <div>
             <div className="sm:px-8 py-16">
-                <h1 className="text-center text-4xl font-bold tracking-tight text-zinc-100 sm:text-5x">Projects I have  develop for now</h1>
+                <h1 className="text-center text-4xl font-bold tracking-tight text-zinc-100 sm:text-5x">Projects I have developed for now</h1>
                 <div className='my-4 flex flex-col lg:flex-row justify-center gap-5 py-4 overflow-hidden sm:gap-8'>
                     <Card title={'Gravadora - Record Company'} subtitle={'This is a project that used the follow technologies:'}
                         img={'/gravadora.png'} 
